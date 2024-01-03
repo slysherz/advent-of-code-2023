5bacf0f743cd0cfb7d687108eeabe565f74b0cbf
Diogo Ribeiro <slysherz@gmail.com>
2024-01-03 16:34:19 +0000

    solve day 21 part 2

--- day21.py
+++ day21.py
@@ -14,7 +14,6 @@ DIR = [
 def add(a, b):
     return (a[0] + b[0], a[1] + b[1])
 
-
 def parse1(input, steps):
     data = parseInput(input)
 
@@ -23,29 +22,57 @@ def parse1(input, steps):
         for x, c in enumerate(line):
             if c == 'S':
                 start = (x, y)
+
+    width = len(data[0])
+    height = len(data)
     
-    reacheable = [start]
+    layers = 6
+    total = [0, 0]
+    visited_even = [set() for i in range(layers)]
+    visited_odd = [set() for i in range(layers)]
+    visited_odd[0].add(start)
+
     for i in range(0, steps):
-        new_reacheable = []
+        if i % 2 == 0:
+            visited = visited_even
+            reacheable = visited_odd[0]
+        else:
+            visited = visited_odd
+            reacheable = visited_even[0]
+
+        visited.insert(0, set())
         for pos in reacheable:
             for dir in DIR:
                 new_pos = add(pos, dir)
                 x, y = new_pos
-                # Skip out of bounds
-                if x < 0 or x >= len(data[0]): continue
-                if y < 0 or y >= len(data): continue
                 # No rocks
-                if data[y][x] == '#': continue
-                # No repeats
-                if new_pos in new_reacheable: continue
-                new_reacheable.append(new_pos)
-        reacheable = new_reacheable
+                if data[y % height][x % width] == '#': continue
+                if any((new_pos in layer) for layer in visited):
+                    continue
+                visited[0].add(new_pos)
 
-    return len(reacheable)
+        retired = visited.pop()
+        total[i % 2] += len(retired)
+
+    if steps % 2 == 1:
+        return sum(len(v) for v in visited_even) + total[0]
+    else:
+        return sum(len(v) for v in visited_odd) + total[1]
 
 def parse2(input):
-    data = parseInput(input)
-    return 0
+    # 26501365 = 202300 * 131 + 65
+    y0 = parse1(input, 65)
+    y1 = parse1(input, 196)
+    y2 = parse1(input, 327)
+    # n489 = parse1(input, 489)
+
+    c = y0
+    a = (y2 - 2 * y1 + c) // 2
+    b = y1 - a - c
+    
+    x = 202300
+    y = a * (x * x) + b * x + c
+    return y
 
 class Test(unittest.TestCase):
     def test_part1(self):
@@ -60,10 +87,6 @@ class Test(unittest.TestCase):
 .##.#.####.
 .##..##.##.
 ...........""", 6), 16)
-        
-    
-    def test_part2(self):
-        self.assertEqual(parse2(""" """), 0)
 
 unittest.main(exit=False)
 
